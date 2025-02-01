730e37ba6799151f745d18453db0c88af9ce1c53
AristosDragatis <dragatis.aris@gmail.com>
2025-02-01 18:46:07 +0200

    new changes
    added some functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <vector>
 using namespace std;
 
 
@@ -36,6 +37,7 @@ public:
         Answer = answer;
     }
 
+
     // show question and answer 
     void Show(){
         if(Question.empty()){
@@ -52,12 +54,12 @@ public:
     }
 
 
+
+
     // save a card to a database SQLite implementation
     void Save(){
 
     }
-
-
 
 private:
     string Question;
@@ -71,8 +73,50 @@ private:
 class Deck{
 public:
     Deck(){}
-    
+    vector<Card> vec; // temp saving the cards 
+
+
+    void add2vec(Card card){
+        vec.push_back(card);
+    }
+
+
+    void print_vec(){
+        if (vec.empty()) {
+        cout << "The deck is empty!" << endl;
+    } else {
+        cout << "Cards in the deck:" << endl;
+        for (auto& card : vec) {
+            card.Show();
+        }
+    }
+    }
+
+
+    // delete a card from the deck
+    void delete_element(Card card){
+        // delete the position of the card
+        for(auto it = vec.begin(); it != vec.end();){
+            if(it ->getQuestion() == card.getQuestion() && it->getAnswer() == card.getAnswer()){
+                it = vec.erase(it); 
+                cout << "Card deleted!" << endl;
+                return;
+            } else {
+                ++it;
+        }
+    }
+        cout << "card not found" << endl;
+    }
+
+
+    // Deleting the deck
+    void delete_vec(){
+        vec.clear();
+        cout << "Deck is deleted!" << endl;
+    }
 };
+
+
 
 // μελέτη με χρονική επανάληψη 
 class Study{
@@ -82,6 +126,8 @@ public:
 };
 
 
+// με timeit θα αποθηκεύω πόσο χρόνο κάνει στην κάθε κάρτα
+// και θα εμφανίζω τα συνολικά στατιστικά και της κάθε κάρτας
 class Statistics{
 public:
     Statistics(){}
@@ -92,10 +138,30 @@ public:
 
 // Main συνάρτηση
 int main() {
+    // creating the objects 
     Card c1;
+    Deck d1;
+    Card c2;
+
+    // setting the questions and answers of the cards
     c1.setQuestion("Hey there?");
     c1.setAnswer("there?");
+    c2.setQuestion("c2 q");
+    c2.setAnswer("c2 a");
+
+
+    // printing the contents of the card
     c1.Show();
+    c2.Show();
+
+    // adding to a deck
+    d1.add2vec(c1);
+    // printing the deck
+    d1.add2vec(c2);
+    d1.print_vec();
+    d1.delete_vec();
+    d1.print_vec();
+
     return 0;
 }
 
